ca00f5587055976122d941f51c1f68fc205e63a7
Matthew Garelli <matthew.garelli@gm.com>
2018-10-17 16:02:44 -0400

    updated cg to be non user specific

---
+++ Scripts/cg.sh
@@ -0,0 +1,3 @@
+g='/usr/bin/git'
+
+command $g --git-dir=$HOME/.cfg/ --work-tree=$HOME "${@}"
--- Scripts/edit/copy.sh
+++ Scripts/edit/copy.sh
@@ -4,5 +4,15 @@ ls='/bin/ls'
 outputFile=$HOME'/Scripts/edit/output.txt'
 vimCommand=$HOME'/Scripts/edit/command.txt'
 
-command "${@}" > $outputFile
+
+
+case "$1" in
+	"here")
+		command echo $PWD > $outputFile
+		;;
+	*)
+		command "${@}" > $outputFile
+		;;
+esac
+
 command $vim $outputFile -s $vimCommand
