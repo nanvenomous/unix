11c6c93e77ca6358cbded0aa0a76fa651f39fbd5
Matthew Garelli <matthew.garelli@gm.com>
2018-10-23 18:46:34 -0400

    created general git wrapper to be repurposed in g.sh and cg.sh

--- Scripts/cliGit/cg.sh
+++ Scripts/cliGit/cg.sh
@@ -1,61 +1,10 @@
 #!/bin/bash
+source ./general.sh
 
 g='/usr/bin/git'
 config="--git-dir=${HOME}/.cfg --work-tree=${HOME}"
 cmd="${g} ${config}"
 
-generalGitWrapper () {
-	local -n cmdRef=$1
-	local allInputs="${@:2}"
-	local shortcut=$2
-	local restOfInputs="${@:3}"
-
-	case "$shortcut" in
-		"b")
-			cmdRef="$cmdRef branch ${restOfInputs}"
-			;;
-		"c")
-			cmdRef="$cmdRef commit ${restOfInputs}"
-			;;
-		"ch")
-			cmdRef="$cmdRef checkout ${restOfInputs}"
-			;;
-		"i")
-			cmdRef="$cmdRef init ${restOfInputs}"
-			;;
-		"l")
-			cmdRef="$cmdRef log ${restOfInputs}"
-			;;
-		"m")
-			cmdRef="$cmdRef merge ${restOfInputs}"
-			;;
-		"p")
-			cmdRef="$cmdRef push ${restOfInputs}"
-			;;
-		"r")
-			cmdRef="$cmdRef remote ${restOfInputs}"
-			;;
-		"s")
-			cmdRef="$cmdRef status ${restOfInputs}"
-			;;
-		"t")
-			cmdRef="$cmdRef ls-tree -r --name-only ${restOfInputs}"
-			;;
-		"chb")
-			cmdRef="$cmdRef checkout -b ${restOfInputs}"
-			;;
-		"pr")
-			cmdRef="$cmdRef pull --rebase ${restOfInputs}"
-			;;
-		"rs")
-			cmdRef="$cmdRef remote show ${restOfInputs}"
-			;;
-		*)
-			cmdRef="$cmdRef ${allInputs}"
-			;;
-	esac
-
-}
 
 case "$1" in
 	"aa")
---
+++ Scripts/cliGit/general.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+generalGitWrapper () {
+	# gather inputs
+	local -n cmdRef=$1
+	local allInputs="${@:2}"
+	local shortcut=$2
+	local restOfInputs="${@:3}"
+
+	# wrapper for shortcuts
+	case "$shortcut" in
+		"b")
+			cmdRef="$cmdRef branch ${restOfInputs}"
+			;;
+		"c")
+			cmdRef="$cmdRef commit ${restOfInputs}"
+			;;
+		"ch")
+			cmdRef="$cmdRef checkout ${restOfInputs}"
+			;;
+		"i")
+			cmdRef="$cmdRef init ${restOfInputs}"
+			;;
+		"l")
+			cmdRef="$cmdRef log ${restOfInputs}"
+			;;
+		"m")
+			cmdRef="$cmdRef merge ${restOfInputs}"
+			;;
+		"p")
+			cmdRef="$cmdRef push ${restOfInputs}"
+			;;
+		"r")
+			cmdRef="$cmdRef remote ${restOfInputs}"
+			;;
+		"s")
+			cmdRef="$cmdRef status ${restOfInputs}"
+			;;
+		"t")
+			cmdRef="$cmdRef ls-tree -r --name-only ${restOfInputs}"
+			;;
+		"chb")
+			cmdRef="$cmdRef checkout -b ${restOfInputs}"
+			;;
+		"pr")
+			cmdRef="$cmdRef pull --rebase ${restOfInputs}"
+			;;
+		"rs")
+			cmdRef="$cmdRef remote show ${restOfInputs}"
+			;;
+		*)
+			cmdRef="$cmdRef ${allInputs}"
+			;;
+	esac
+}
