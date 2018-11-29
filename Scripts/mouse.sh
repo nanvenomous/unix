b52fc1ecd573f0f99b0062ceab0967a0ffdfce1c
Matthew <mrgarelli@gmail.com>
2018-11-29 15:42:26 -0500

    updated mouse to just remove trackpad, will still need to handle error when trackpad does not exist in system

--- Scripts/mouse.sh
+++ Scripts/mouse.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Below gets id=### from xinput
+id="$(xinput | grep 'DLL07BE:01 06CB:7A13 Touchpad' | awk '{print $6}')"
+# next we remove the id= part to just get 11
+id="${id/'id='/}"
+
 case "$1" in
 	"on")
-		command xinput enable 11 "${@:2}"
+		command xinput enable ${id} "${@:2}"
 		;;
 	"off")
-		command xinput disable 11 "${@:2}"
+		command xinput disable ${id} "${@:2}"
 		;;
 esac
