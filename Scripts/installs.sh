37dd9531e5581b7a0880fb30e360eb9eccd760ff
Matthew Garelli <mgarelli@alumni.stanford.edu>
2019-03-31 13:23:36 -0400

    added install file to quickly configure system

---
+++ Scripts/installs.sh
@@ -0,0 +1,23 @@
+# All installs that we have the repos for
+sudo apt-get install chromium-browser
+sudo apt-get install vim
+sudo apt-get install vim-gnome
+sudo apt-get install tree
+sudo apt-get install konsole
+sudo apt-get install unclutter
+sudo apt-get install xsel
+sudo apt-get install
+sudo apt-get install
+sudo apt-get install
+sudo apt-get install
+sudo apt-get install
+sudo apt-get install
+
+# Add all repositories!!
+sudo add-apt-repository ppa:synapse-core/ppa
+
+# Run the installer update
+sudo apt-get update
+
+# Now run the installs
+sudo apt-get install synapse
