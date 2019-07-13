b7bee556e417d1781dbaf055e74419d5fd387629
pablo <mgarelli@alumni.stanford.edu>
2019-07-13 12:02:58 -0400

    bash symlink script is now pure bash for simplicity

--- Scripts/symlinks/files/b.sh
+++ Scripts/symlinks/files/b.sh
@@ -1,25 +1,61 @@
 #!/bin/bash
 
+# Programs
 bsh='/bin/bash'
 v='/usr/bin/vim'
+
+# Configuration Files
 conf="${HOME}/.bashrc"
 profile="${HOME}/.profile"
 locConf="${HOME}/.locrc"
 
-case "${1}" in
-	"-e")
-		${v} ${conf}
-		;;
-	"-el")
-		${v} ${locConf}
-		;;
-	"-ep")
-		${v} ${profile}
-		;;
-	"-s")
-		exec ${bsh}
-		;;
-	*)
-		${bsh} "${@}"
-		;;
-esac
+# Variables
+help=$(cat <<-END
+Wrapper for bash command
+
+options
+\t-e: edit .bashrc
+\t-l: edit .locrc
+\t-p: edit .profile
+\t-h: help menu
+\t-s: exec bash
+
+argument
+\tshell script to run
+END
+)
+
+while getopts ":eh" opt; do
+	case "${opt}" in
+		e )
+			${v} ${conf}
+			exit 0
+			;;
+		l )
+			${v} ${locConf}
+			exit 0
+			;;
+		p )
+			${v} ${profile}
+			exit 0
+			;;
+		h ) 
+			echo -e "${help}"
+			exit 0
+			;;
+		s )
+			exec ${bsh}
+			exit 0
+			;;
+		/? )
+			echo -e "Invalid Option: -${OPTARG}"
+			exit 1
+			;;
+		* )
+			${bsh} "${@}"
+			exit 0
+			;;
+	esac
+done
+
+echo -e "${help}"
