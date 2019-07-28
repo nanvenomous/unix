2bf0d5a6c7ae7aadd41a586e3447053af574a339
pablo <mgarelli@alumni.stanford.edu>
2019-07-28 11:55:34 -0400

    migrated git abbreviations to the new methodology

--- .bashrc
+++ .bashrc
@@ -51,8 +51,6 @@ alias src="bash ${HOME}/scripts/scripts.sh; source ${HOME}/.bashrc"
 #________________________________________________________________________________
 #__________Scripts
 alias apt-size='bash ~/Scripts/apt-size.sh'
-alias g='bash ~/Scripts/git/g.sh'
-alias cg='bash ~/Scripts/git/cg.sh'
 alias sl='sudo bash ~/Scripts/symlinks/sl.sh'
 alias settings='bash ~/.settings/settings.sh'
 alias pypi='bash ~/Scripts/pypi/run.sh'
---
+++ scripts/src/g/addAllConfigFiles.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cmd="${@}"
+
+# add/stage changes in all configuration files
+${cmd} add ${HOME}'/.bashrc'
+${cmd} add ${HOME}'/.vimrc'
+${cmd} add ${HOME}'/.profile'
+${cmd} add ${HOME}'/.xinitrc'
+${cmd} add ${HOME}'/.inputrc'
+${cmd} add ${HOME}'/.gitignore'
+${cmd} add ${HOME}'/.settings/*'
+${cmd} add ${HOME}'/Scripts/*'
+${cmd} add ${HOME}'/scripts/src/*'
+${cmd} add ${HOME}'/scripts/scripts.sh'
+${cmd} add ${HOME}'/scripts/to_link.py'
+
+${cmd} add ${HOME}'/.local/share/konsole/*'
+${cmd} add ${HOME}'/.config/konsolerc'
+${cmd} add ${HOME}'/.config/Code/User/keybindings.json'
+${cmd} add ${HOME}'/.config/Code/User/settings.json'
+${cmd} add ${HOME}'/.config/openbox/lxde-rc.xml'
