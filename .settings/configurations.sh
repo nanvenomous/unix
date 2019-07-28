6f9d531af5b76dec16365ce320a7342a619dfa37
pablo <mgarelli@alumni.stanford.edu>
2019-07-28 15:33:53 -0400

    added sudoers file editing to the configuration script

--- .settings/configurations.sh
+++ .settings/configurations.sh
@@ -1,15 +1,16 @@
 # VARIABLES
 h="${HOME}"
 
-# SYMLINKS
-"${h}/Scripts/symlinks/sl.sh" link
-
 # CREATE DESIRED DIRECTORIES
-mkdir "${h}/Projects"
-mkdir "${h}/Images"
+mkdir -p "${h}/Projects"
+mkdir -p "${h}/Images"
 
 # REMOVE UNWANTED DIRECTORIES
 toRemove=( 'Desktop' 'Documents' 'Music' 'Pictures' 'Public' 'Templates' 'Videos' )
 for dir in "${toRemove[@]}"; do
 	rmdir "${h}/${dir}"
 done
+
+echo "press enter to edit sudoers file"
+read -n 1 -p "Input Selection:" mainmenuinput
+sudo visudo
