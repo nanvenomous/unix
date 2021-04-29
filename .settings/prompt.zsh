8123d679872565de50cb2496b81e2fe17948805b
Matthew Garelli <matthew.garelli@gm.com>
2021-04-28 18:27:03 -0600

    MG | added user and hostname to rprompt

--- .settings/prompt.zsh
+++ .settings/prompt.zsh
@@ -1,9 +1,17 @@
+oBrack='%F{magenta}{%f'
+cBrack='%F{magenta}}%f'
+oParen='%F{blue}(%f'
+cParen='%F{blue})%f'
+at='%F{yellow}@%f'
+
 autoload -Uz vcs_info
 setopt prompt_subst
-zstyle ':vcs_info:git:*' formats '(%b)'
+zstyle ':vcs_info:git:*' formats "${oParen}%b${cParen}"
 zstyle ':vcs_info:*' enable git
+
 precmd() {
 	vcs_info
-	print -rP "%B%F{cyan}%~%f%b %F{blue}${vcs_info_msg_0_}%f"
+	print -rP "%B%F{cyan}%~%f%b ${vcs_info_msg_0_}"
 }
+RPROMPT="${oBrack}%n${at}%m${cBrack}"
 PROMPT='%(?.%F{green}0.%F{red}%?)%f %# '
