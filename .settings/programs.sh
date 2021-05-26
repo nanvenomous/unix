d2d0ba194312531181766acd21965d88f48a7a77
Matthew Garelli <matthew.garelli@gm.com>
2021-05-25 22:54:02 -0500

    MG | mac now has it's own config for nvm, android

--- .settings/programs.sh
+++ .settings/programs.sh
@@ -11,20 +11,28 @@ alias keychain='sudo pacman -S archlinux-keyring; sudo pacman-key --populate arc
 case "$(uname -s)" in
 	Linux*)
 		source /usr/share/nvm/init-nvm.sh
+		export ANDROID_SDK_ROOT='/opt/android-sdk'
+		export ANDROID_HOME='/opt/android-sdk'
+		export ANDROID_AVD_HOME="${HOME}/.android/avd"
+		export JAVA_HOME='/usr/lib/jvm/default'
 		;;
 	Darwin*)
 		export NVM_DIR="$HOME/.nvm"
 		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+		export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
+		export ANDROID_HOME="${HOME}/Library/Android/sdk"
+		export ANDROID_SDK="${ANDROID_HOME}"
+		export ANDROID_SDK_ROOT="${ANDROID_HOME}"
+		export PATH="$PATH:$ANDROID_HOME/emulator"
+		export PATH="$PATH:$ANDROID_HOME/tools"
+		export PATH="$PATH:$ANDROID_HOME/tools/bin"
+		export PATH="$PATH:$ANDROID_HOME/platform-tools"
+
 		;;
 esac
 
 export GOROOT=/usr/lib/go
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-
-export ANDROID_SDK_ROOT='/opt/android-sdk'
-export ANDROID_HOME='/opt/android-sdk'
-export ANDROID_AVD_HOME="${HOME}/.android/avd"
-
-export JAVA_HOME='/usr/lib/jvm/default'
