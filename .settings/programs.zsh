ddab808bec9732a521adc7ff4c957ebf5e28441b
Matthew Garelli <mrgarelli@gmail.com>
2021-10-14 05:30:45 +0000

    added alias to surf the web and adjusted indentation

--- .settings/programs.zsh
+++ .settings/programs.zsh
@@ -8,54 +8,61 @@ alias dsk="lsblk | grep -v -e 'SWAP' -e 'loop'"
 alias ss='scrot' # scrot -d 20
 alias keychain='sudo pacman -S archlinux-keyring; sudo pacman-key --populate archlinux; sudo pacman-key --refresh'
 
+function _goog() {
+  # surf "https://www.google.com/search?q=$(printf %q ${@})" &
+  query="https://www.google.com/search?q=${@}"
+  surf "${query}" &
+}
+alias goog=_goog
+
 function _hr() {
-	echo "${PWD}"
+  echo "${PWD}"
 }
 alias hr=_hr
 
 PATH="$PATH:${HOME}/.local/bin"
 
 case "$(uname -s)" in
-	Linux*)
-		################### NVM
-		source /usr/share/nvm/init-nvm.sh
+  Linux*)
+    ################### NVM
+    source /usr/share/nvm/init-nvm.sh
 
-		################### JAVA
-		export JAVA_HOME='/usr/lib/jvm/default'
+    ################### JAVA
+    export JAVA_HOME='/usr/lib/jvm/default'
 
-		################### ANDROID
-		export ANDROID_SDK_ROOT='/opt/android-sdk'
-		export ANDROID_HOME='/opt/android-sdk'
-		export ANDROID_AVD_HOME="${HOME}/.android/avd"
+    ################### ANDROID
+    export ANDROID_SDK_ROOT='/opt/android-sdk'
+    export ANDROID_HOME='/opt/android-sdk'
+    export ANDROID_AVD_HOME="${HOME}/.android/avd"
 
-		################### GO
-		export GOROOT=/usr/lib/go
-		export GOPATH=$HOME/go
-		export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-		;;
-	Darwin*)
-		################### NVM
-		export NVM_DIR="$HOME/.nvm"
-		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    ################### GO
+    export GOROOT=/usr/lib/go
+    export GOPATH=$HOME/go
+    export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+    ;;
+  Darwin*)
+    ################### NVM
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-		################### JAVA
-		# export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
-		export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_202.jdk/Contents/Home'
+    ################### JAVA
+    # export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
+    export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_202.jdk/Contents/Home'
 
-		################### ANDROID
-		export ANDROID_HOME="${HOME}/Library/Android/sdk"
-		export ANDROID_SDK="${ANDROID_HOME}"
-		export ANDROID_SDK_ROOT="${ANDROID_HOME}"
-		export PATH="$PATH:$ANDROID_HOME/emulator"
-		export PATH="$PATH:$ANDROID_HOME/tools"
-		export PATH="$PATH:$ANDROID_HOME/tools/bin"
-		export PATH="$PATH:$ANDROID_HOME/platform-tools"
+    ################### ANDROID
+    export ANDROID_HOME="${HOME}/Library/Android/sdk"
+    export ANDROID_SDK="${ANDROID_HOME}"
+    export ANDROID_SDK_ROOT="${ANDROID_HOME}"
+    export PATH="$PATH:$ANDROID_HOME/emulator"
+    export PATH="$PATH:$ANDROID_HOME/tools"
+    export PATH="$PATH:$ANDROID_HOME/tools/bin"
+    export PATH="$PATH:$ANDROID_HOME/platform-tools"
 
-		################### GO
-		export GOPATH=$HOME/go
-		export GOROOT=/usr/local/opt/go/libexec
-		export PATH=$PATH:$GOPATH/bin
-		export PATH=$PATH:$GOROOT/bin
-		;;
+    ################### GO
+    export GOPATH=$HOME/go
+    export GOROOT=/usr/local/opt/go/libexec
+    export PATH=$PATH:$GOPATH/bin
+    export PATH=$PATH:$GOROOT/bin
+    ;;
 esac
