20f63b0d316483f80708b05927e9f5f2ecd49558
nanvenomous <mrgarelli@gmail.com>
2026-04-28 09:41:34 -0500

    add: support for remote clipboard

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,19 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.opt.clipboard = 'unnamedplus'
+if vim.env.SSH_TTY ~= nil then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+end
 vim.opt.cursorline = true
 
 local keymap = vim.keymap.set
